600400649c05445390c8eda896b3e74ff86686ae
ismailnajah <49298006+ismailnajah@users.noreply.github.com>
2020-07-20 13:35:50 +0100

    UI update

--- PingPackage/Main.java
+++ PingPackage/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("layout.fxml"));
-        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("../Assets/icon.png")));
+        Parent root = FXMLLoader.load(getClass().getResource("./layout.fxml"));
+        primaryStage.getIcons().add(new Image(Main.class.getResourceAsStream("Assets/icon.png")));
         primaryStage.setTitle("Ping Riot Servers");
         primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
